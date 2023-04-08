236feca23c5043e052a742449df1732fd1d0e7c0
Djuslun <djuslun@yandex.ru>
2023-04-08 12:08:37 +0300

    fix label bug

--- src/components/formNote/CustomField.js
+++ src/components/formNote/CustomField.js
@@ -1,17 +1,18 @@
 import { Field, ErrorMessage } from "formik"
 
 export const CustomField = ({ label, edit, noteId, ...props }) => {
-  const { id } = props
   return (
     <>
       <div className="form-note__input">
-        <label className="form-note__label" htmlFor={id}>{label}</label>
-        <Field
-          className='form-note__field'
-          disabled={noteId ? !edit : false}
-          {...props}
-        />
-        <ErrorMessage name={props.name}>{msg => <div className="error">{msg}</div>}</ErrorMessage>
+        <label >
+          <p className="form-note__label">{label}</p>
+          <Field
+            className='form-note__field'
+            disabled={noteId ? !edit : false}
+            {...props}
+          />
+          <ErrorMessage name={props.name}>{msg => <div className="error">{msg}</div>}</ErrorMessage>
+        </label>
       </div>
     </>
   )
