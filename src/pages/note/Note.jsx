9067f37d35afcdae59c5c81b06a1b831ef5891d6
Djuslun <djuslun@yandex.ru>
2023-06-07 09:44:45 +0300

    add redirect on 404 when note page no exist, redirect on /notes when note deleted

--- src/pages/note/Note.jsx
+++ src/pages/note/Note.jsx
@@ -1,14 +1,22 @@
 import FormNote from '../../components/formNote/FormNote'
-import { useCallback } from 'react'
+import { useCallback, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { notesDelete } from '../../redux/notes.Slice'
 import { useParams } from 'react-router'
 import { selectById } from '../../redux/notes.Slice'
+import { useNavigate } from 'react-router'
 
 const Note = ({ }) => {
   const { id: noteId } = useParams()
-  const { title, description, tags } = useSelector(state => selectById(state, noteId))
+  const { title, description, tags } = useSelector(state => selectById(state, noteId)) || {}
   const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (!title || !tags) {
+      navigate('/not_found')
+    }
+  }, [title, tags])
 
   const handleDelete = useCallback(() => {
     dispatch(notesDelete(noteId))
