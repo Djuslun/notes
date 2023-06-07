eb9c210e7fb17a19fbd81355a02bfd0e341c7f9c
Djuslun <djuslun@yandex.ru>
2023-06-07 08:47:28 +0300

    add not found page

--- src/pages/notFoundPage/NotFoundPage.jsx
+++ src/pages/notFoundPage/NotFoundPage.jsx
@@ -1,11 +1,14 @@
-
+import { Link } from "react-router-dom"
+import './notFoundPage.scss'
 
 const NotFoundPage = ({ }) => {
 
   return (
-    <>
-      <h1>Not found</h1>
-    </>
+    <div className="not-found-page">
+      <h2 className="not-found-page__title" >404</h2>
+      <p className="not-found-page__subtitle">page not found</p>
+      <Link className="not-found-page__link" to={'/'}>Back to main page</Link>
+    </div>
   )
 }
 
